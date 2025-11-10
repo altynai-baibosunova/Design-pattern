4a05b3e3af37bec40659b48a4daa39d67af356e8
Altynai <harmoni5413@gmail.com>
2025-11-10 14:25:44 -0500

    modified main

--- src/main.java
+++ src/main.java
@@ -1,20 +1,9 @@
 public class main {
-    public static void main(String[] args) {
-        CacheSingleton singleton = CacheSingleton.getInstance(3);
-        LRUCache cache = singleton.getCache();
-        cache.put("A", 1);
-        cache.put("B", 2);
-        cache.put("C", 3);
-        cache.printCache(); // {A=1, B=2, C=3}
+        public static void main(String[] args) {
+            LRUCache cache = CacheFactory.createLRUCache(5);
+            cache.put("A", 1);
+            cache.put("B", 2);
 
-        cache.get("A"); // Access A → becomes most recently used
-        cache.put("D", 4); // Evicts least recently used (B)
-        cache.printCache(); // {C=3, A=1, D=4}
-
-        // Access the singleton again elsewhere
-        CacheSingleton sameSingleton = CacheSingleton.getInstance(3);
-        LRUCache sameCache = sameSingleton.getCache();
-        sameCache.put("E", 5);
-        sameCache.printCache(); // {A=1, D=4, E=5}
-    }
+            System.out.println(cache.get("A")); // Output: 1
+        }
 }
